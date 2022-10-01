7baafe40c2ce0e9a1ca1f4d200c112b7beec2aae
suleman <suleman12feb97@gmail.com>
2022-10-01 02:09:58 +0000

    app configurations updated

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,6 +8,17 @@ class Settings(BaseSettings):
     NASA_IMAGE_AND_VIDEO_LIBRARY_API_URL: str = ''
     NRTS_API_URL: str = ''
 
+    ML_MODEL_KEY_1: str = ''
+    ML_MODEL_KEY_2: str = ''
+    ML_MODEL_KEY_3: str = ''
+    ML_MODEL_KEY_4: str = ''
+    ML_MODEL_KEY_5: str = ''
+    ML_MODEL_KEY_6: str = ''
+    ML_MODEL_KEY_7: str = ''
+    ML_MODEL_KEY_8: str = ''
+
+    SUMMARY_MODEL_SERVER_URL: str = ''
+
     class Config:
         # All settings can be replaced with the contents in the '.env' file
         env_file: str = ".env"
